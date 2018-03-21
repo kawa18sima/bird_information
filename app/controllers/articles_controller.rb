1ce1fbb7a2405fd9dbc78a6f0802c24899f9a621
Masaya Nakashima <kawa18sima@gmail.com>
2018-03-22 00:09:35 +0900

    Article archive function completed
    Article archive function completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -69,6 +69,6 @@ class ArticlesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def article_params
-      params.require(:article).permit(:title, :place, :complete)
+      params.require(:article).permit(:title, :article).merge(complete: true)
     end
 end
