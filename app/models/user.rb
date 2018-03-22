bc625912b040a2330413b6e67088afac6823c544
Masaya Nakashima <kawa18sima@gmail.com>
2018-03-22 20:05:54 +0900

    Fix login
    Fix login

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,18 +2,18 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :authentication_keys => [:login]
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   attr_accessor :login
 
-  def self.find_first_by_auth_conditions(warden_conditions)
-    conditions = warden_conditions.dup
-    if login = conditions.delete(:login)
-      where(conditions).where(["name = :value OR lower(email) = lower(:value)", { :value => login }]).first
-    else
-      where(conditions).first
-    end
-  end
+  # def self.find_first_by_auth_conditions(warden_conditions)
+  #   conditions = warden_conditions.dup
+  #   if login = conditions.delete(:login)
+  #     where(conditions).where(["name = :value OR lower(email) = lower(:value)", { :value => login }]).first
+  #   else
+  #     where(conditions).first
+  #   end
+  # end
 
   validates :name,
   uniqueness: { case_sensitive: :false },
