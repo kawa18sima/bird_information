fa1d1e5d900d5ecd3af121f0e4dc7cf27f93fa18
Masaya Nakashima <kawa18sima@gmail.com>
2018-03-22 20:29:54 +0900

    Add Relation
    How to link a user with an article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -69,6 +69,6 @@ class ArticlesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def article_params
-      params.require(:article).permit(:title, :article).merge(complete: true)
+      params.require(:article).permit(:title, :article).merge(complete: true, user_id: current_user.id)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,16 +6,9 @@ class User < ApplicationRecord
 
   attr_accessor :login
 
-  # def self.find_first_by_auth_conditions(warden_conditions)
-  #   conditions = warden_conditions.dup
-  #   if login = conditions.delete(:login)
-  #     where(conditions).where(["name = :value OR lower(email) = lower(:value)", { :value => login }]).first
-  #   else
-  #     where(conditions).first
-  #   end
-  # end
-
   validates :name,
   uniqueness: { case_sensitive: :false },
   length: { minimum: 4, maximum: 20 }
+
+  has_many :articles
 end
