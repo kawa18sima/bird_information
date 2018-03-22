746f1952fd13e42f25b7579128e8467365249763
Masaya Nakashima <kawa18sima@gmail.com>
2018-03-23 01:26:27 +0900

    add mypage
    Mypage function

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   resources :articles
   root 'articles#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [] do
+    collection do
+      get 'mypage'
+    end
+  end
 end
