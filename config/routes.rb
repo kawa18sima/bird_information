816f4a70ccb97ce6d955b268fd4608b3a324ab83
Masaya Nakashima <kawa18sima@gmail.com>
2018-03-27 21:01:00 +0900

    Search function added
    Add search field in header
    The result is displayed.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :articles
+  resources :articles do
+    collection do
+      get 'search'
+    end
+  end
+
   root 'articles#index'
   resources :users, only: [] do
     collection do
